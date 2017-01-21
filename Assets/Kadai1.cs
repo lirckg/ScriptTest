7f03174418a58c1a331189ae2d8a9176cb25299f
Ashidariku <lirckm@gmail.com>
2017-01-22 00:06:43 +0900

    ScriptTestプロジュエクト　初めてのコミット
    Lesson4で作製したファイルになります。課題も完了しています。

---
+++ Assets/Kadai1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kadai1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int[] array = { 10, 20, 40, 30, 90 };
+
+		//順番に表示
+		for (int i = 0; i < array.Length; i++) {
+			Debug.Log (array [i]);
+		}
+
+		//逆順に表示
+		for (int i = array.Length - 1; i >= 0; i--) {
+			Debug.Log (array [i]);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Kadai2.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss2{
+	private int hp = 100;	//体力
+	private int power = 25;	//攻撃力
+	private int mp = 53;	//魔法
+
+	// 攻撃用の関数
+	public void Attack(){
+		Debug.Log (this.power + "のダメージを与えた");
+	}
+
+	// 防御用の関数
+	public void Defence(int damage){
+		Debug.Log (damage + "のダメージを受けた");
+		// 残りhpを減らす
+		this.hp -= damage;
+	}
+
+	// 魔法用の関数
+	public void Magic(){
+		if (this.mp >= 5) {
+			this.mp -= 5;
+			Debug.Log ("魔法攻撃をした。残りMPは" + this.mp + "。");
+		}else{
+			Debug.Log ("MPが足りないため魔法が使えない。");
+		}
+	}
+}
+
+public class Kadai2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		Boss2 boss = new Boss2 ();
+
+		for (int i = 0; i < 15; i++) {
+			boss.Magic ();
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
